3c3d9e2363fa637215f255a2b8f9e92e61258e8d
Szymon <ssikora258@gmail.com>
2025-06-24 22:19:31 +0200

    fix: prod prep delete unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import { LanguageProvider } from './context/LanguageContext'
 import Layout from './components/Layout'
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useTheme } from '../context/ThemeContext'
 import { Button } from '../components/Button'
 
